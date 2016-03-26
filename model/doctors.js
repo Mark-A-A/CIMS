ddf8557b8da327ec52b0ccbe3d6ab73800a69f98
C-Das <chinmay98@gmail.com>
2016-03-26 13:44:04 -0400

    re-arranged the model folder

--- model/doctors.js
+++ model/doctors.js
@@ -1,1 +1,14 @@
-//mongoose model for doctors
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var doctorSchema = {
+  name: String,
+  specialty: String,
+  location: {
+    city: String,
+    state: String
+  },
+};
+
+var Doctor = mongoose.model('Doctor', doctorSchema);
+module.exports = Doctor;
