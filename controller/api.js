72af9f7b88afc1808876795fbeaa4ca8798b8031
C-Das <chinmay98@gmail.com>
2016-03-29 00:58:28 -0400

    Integrated login and register in CIMS application

---
+++ controller/api.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+router.use(function(req,res,next){
+
+  if(req.method === "GET") {
+    return next();
+  }
+
+  if(!req.isAuthenticated()){
+    //user not authenticated, redirect to login page
+    return res.redirect('/#login');
+  }
+  //user authenticated redirect to next page
+  return next();
+})
+
+module.exports = router;
--- public/javascripts/doctor.js
+++ public/javascripts/doctor.js
@@ -1,5 +1,72 @@
-var app = angular.module('doctorPage', []);
+var app = angular.module('doctorsPage', ['ngRoute']).run(function($rootScope,$http){
+
+      $rootScope.authenticated = false;
+      $rootScope.current_user = " ";
+
+      $rootScope.logout = function(){
+        $http.get('/auth/signout');
+
+        $rootScope.authenticated = false;
+        $rootScope.current_user = " ";
+      }
+// });
+
+app.config(function($routeProvider){
+  $routeProvider
+   //the timeline display
+   .when('/', {
+    templateUrl: 'main.html',
+    controller: 'mainController'
+   })
+   //the login display
+   .when('/login', {
+    templateUrl:'login.html',
+    controller: 'authController'
+   })
+   //the signup display
+   .when('/signup', {
+    templateUrl:'register.html',
+    controller:'authController'
+   })
+})
 
 app.controller('mainController', function($scope){
+
+  //Enter main content
+
+});
+
+app.controller("authController",function($scope, $rootScope, $http, $location){
+  $scope.user = {username:'',password:''};
+  $scope.error_message ='';
+
+  $scope.login = function (){
+    $http.post('/auth/login', $scope.user).success(function(data){
+
+      if(data.state === 'success'){
+        $rootScope.authenticated = true;
+        $rootScope.current_user = data.username;
+        $location.path('/');
+      } else {
+        $scope.error_message = data.message;
+      }
+    });
+  };
+
+  $scope.register = function(){
+      console.log("called register function");
+
+      $http.post('/auth/signup', $scope.user).success(function(data){
+        if(data.state === 'success'){
+          $rootScope.authenticated = true;
+          $rootScope.current_user = data.username;
+          console.log("successfully registered");
+          $location.path('/');
+        }else {
+          $scope.error_message = data.message;
+        }
+    });
+  };
+});
 
 });
