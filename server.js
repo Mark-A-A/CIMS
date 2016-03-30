b05f37b1ee9d9354ee2bad34ae204848ecc8655b
Ivan <ivan.baena04@gmail.com>
2016-03-30 05:16:31 -0400

    bootstrapped login and register pages

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var session = require('express-session');
-var mongoose = require('mongoose');
 
 var PORT = 3000;
 
