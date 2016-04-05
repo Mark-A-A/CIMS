82b4db5ffc4f06ba86d43e9966287d50b478952b
Servio <servio19@gmail.com>
2016-04-05 17:50:21 -0400

    more scraper code for another site with images to display

--- public/javascripts/scraper.js
+++ public/javascripts/scraper.js
@@ -2,7 +2,9 @@ var cheerio = require('cheerio');
 var request = require('request');
 var Article = require('../model/articles');
 
-request('http://http://www.webmd.com/news/', function(err, response, body){
+// not sure which site we will scrape from, but this is the code we need for both
+
+request('http://www.webmd.com/news/', function(err, response, body){
   if (!error && response.statusCode == 200){
 
     $ = cheerio.load(body);
@@ -25,5 +27,19 @@ request('http://http://www.webmd.com/news/', function(err, response, body){
       });
     });
   } // end if statement
-  res.send('Scrape Done!')
 }); // end http scrape
+
+request('http://www.news-medical.net/', function(err, response, body){
+  if (!error && response.statusCode == 200) {
+    $ = cheerio.load(body);
+
+    $('.common-img-list').each(function(i, element){
+      $(element).find('li').each(function(){
+        var img = $(this).find('img').attr("src");
+        var text = $(this).find('.common-img-list-text').text();
+      });
+
+      // add code to save to db
+    });
+  }
+});
