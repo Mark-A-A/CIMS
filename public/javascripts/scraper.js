88f90021245dbbeee6cfa45bf6b71a233b57b033
Servio <servio19@gmail.com>
2016-04-05 18:00:36 -0400

    fixed var items because I made a mistake and was not grabbing correct items for scrape

--- public/javascripts/scraper.js
+++ public/javascripts/scraper.js
@@ -35,8 +35,9 @@ request('http://www.news-medical.net/', function(err, response, body){
 
     $('.common-img-list').each(function(i, element){
       $(element).find('li').each(function(){
-        var img = $(this).find('img').attr("src");
+        var img = $(this).find('img').attr('src');
         var text = $(this).find('.common-img-list-text').text();
+        var link = $(this).find('.common-img-list-text').attr('href')
       });
 
       // add code to save to db
