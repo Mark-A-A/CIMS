d2422005920dea0761147a190dedd3b621f94fd5
Ivan <ivan.baena04@gmail.com>
2016-04-06 01:12:48 -0400

    set up search input to search for doctors by entering zip

--- public/ang-Ctrl/searchCtrl.js
+++ public/ang-Ctrl/searchCtrl.js
@@ -9,16 +9,23 @@ searchCtrl.controller('searchBar', ['$scope', '$http', function($scope, $http) {
   };
 
   $scope.submitSearch = function() {
-    $http.get('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&sort=rating-desc&skip=0&limit=3&user_key=c77db2625ba3d0debf3e9be3b74158bd').success(function(data) {
-      console.log(data.data);
-      $scope.doctors = data.data;
+    $http.get('http://maps.googleapis.com/maps/api/geocode/json?address=' + $scope.zip).success(function(data) {
+
+      $scope.userLat = data.results[0].geometry.location.lat;
+      $scope.userLon = data.results[0].geometry.location.lng;
+      console.log(data.results[0].geometry.location);
+      $http.get('https://api.betterdoctor.com/2016-03-01/doctors?location=' + data.results[0].geometry.location.lat + '%2C' + data.results[0].geometry.location.lng + '%2C100&user_location=' + data.results[0].geometry.location.lat + '%2C' + data.results[0].geometry.location.lng + '&sort=best-match-desc&skip=0&limit=3&user_key=c77db2625ba3d0debf3e9be3b74158bd').success(function(data) {
+        console.log(data.data);
+        $scope.doctors = data.data;
+      });
+
     });
+
   };
 }]);
 
 searchCtrl.controller('doctorDetails', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
-
-  $http.get('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&sort=rating-desc&skip=0&limit=3&user_key=c77db2625ba3d0debf3e9be3b74158bd').success(function(data) {
+  $http.get('https://api.betterdoctor.com/2016-03-01/doctors?location=nj-hoboken&user_location=40.500%2C77.413&sort=rating-desc&skip=0&limit=3&user_key=c77db2625ba3d0debf3e9be3b74158bd').success(function(data) {
     console.log(data.data);
     $scope.details = data.data;
     $scope.whichItem = $stateParams.id;
