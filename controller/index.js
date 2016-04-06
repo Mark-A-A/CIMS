5770d9de495907333dfc5fd0a29c58a61d2ef626
Servio <servio19@gmail.com>
2016-04-05 21:08:13 -0400

    added route for getting articles from scraper

--- controller/index.js
+++ controller/index.js
@@ -7,4 +7,11 @@ router.get('/', function(req, res, next) {
   res.send('/public/views/index.html');
 });
 
+router.get('/articles', function(req, res, next) {
+  db.articles.find({},function (err, docs) {
+    //console.log(docs);
+    res.json(docs);
+  });
+});
+
 module.exports = router;
