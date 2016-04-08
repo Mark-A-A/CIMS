171eac7ad02236a56b06acccbac256cb1e0c5880
Servio <servio19@gmail.com>
2016-04-08 15:53:08 -0400

    added scraper code to route then going to use angular to populate after confirming everything works

--- controller/index.js
+++ controller/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var cheerio = require('cheerio');
+var request = require('request');
+var Article = require('.../model/articles');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,10 +11,47 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/articles', function(req, res, next) {
-  db.articles.find({},function (err, docs) {
-    //console.log(docs);
-    res.json(docs);
-  });
+  request('http://www.webmd.com/news/', function(err, response, body){
+    if (!error && response.statusCode == 200){
+
+      $ = cheerio.load(body);
+
+      $('#more-news').each(function(i, element){
+        $(element).find('li').each(function(){
+          var link = $(this).attr('href') ;
+          var title = $(this).text();
+        });
+
+        var article = new Article({
+          title: title,
+          link: link
+        });
+
+        article.save(function(err, document){
+          if(err) {
+            return res.send("ERROR: " + err);
+          }
+        });
+      });
+    } // end if statement
+    res.send('Scrape Done!');
+  }); // end http scrape
+
+  // request('http://www.news-medical.net/', function(err, response, body){
+  //   if (!error && response.statusCode == 200) {
+  //     $ = cheerio.load(body);
+
+  //     $('.common-img-list').each(function(i, element){
+  //       $(element).find('li').each(function(){
+  //         var img = $(this).find('img').attr('src');
+  //         var text = $(this).find('.common-img-list-text').text();
+  //         var link = $(this).find('.common-img-list-text').attr('href')
+  //       });
+
+  //       // add code to save to db
+  //     });
+  //   }
+  // });
 });
 
 module.exports = router;
