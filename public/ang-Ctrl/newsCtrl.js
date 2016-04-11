729f2bd227c7f02f37d63d722e48928362d10788
Servio <servio19@gmail.com>
2016-04-11 11:42:03 -0400

    fixed minor issue with newsCtrl

--- public/ang-Ctrl/newsCtrl.js
+++ public/ang-Ctrl/newsCtrl.js
@@ -2,6 +2,6 @@ var newsCtrl = angular.module('newsCtrl', ['ngAnimate']);
 
 newsCtrl.controller('loadArticles', ['$scope', '$http', function($scope, $http) {
   $http.get('/scraper').success(function(response){
-    $scope.articles = response;
+    $scope.articles = response.data;
   });
 }]);
