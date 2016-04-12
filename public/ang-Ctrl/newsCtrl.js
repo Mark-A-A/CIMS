45189dc1afb0e5b7e82ee2ec1ca97d3a8a2d2f63
Servio <servio19@gmail.com>
2016-04-11 21:37:53 -0400

    got angualr to display datagit add .git add .git add .

--- public/ang-Ctrl/newsCtrl.js
+++ public/ang-Ctrl/newsCtrl.js
@@ -5,8 +5,8 @@ newsCtrl.controller('articleController', ['$scope', '$http', function($scope, $h
 
   $http.get('/scraper').success(function(data){
     for (var i = 0; i < data.length; i++) {
-      console.log(data[i]);
-      //$scope.articles = data[i];
-    };
+      //console.log(data[i]);
+      $scope.articles.push(data[i]);
+    }
   });
 }]);
