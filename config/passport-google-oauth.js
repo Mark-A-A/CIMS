1946c5c0af686c2c8efe41ee287db9b97ca921b5
Mark-A-A <markawai01@gmail.com>
2016-04-15 10:45:17 -0400

    cleaning up code

--- config/passport-google-oauth.js
+++ config/passport-google-oauth.js
@@ -14,14 +14,14 @@ module.exports = function(passport) {
 
     // used to serialize the user for the session
     passport.serializeUser(function(user, done) {
-        done(null, user.id);
+      done(null, user.id);
     });
 
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user) {
-            done(err, user);
-        });
+      User.findById(id, function(err, user) {
+        done(err, user);
+      });
     });
     
     // code for login (use('local-login', new LocalStategy))
@@ -38,50 +38,51 @@ module.exports = function(passport) {
     //   Strategies in Passport require a `verify` function, which accept
     //   credentials (in this case, an accessToken, refreshToken, and Google
     //   profile), and invoke a callback with a user object.
-    passport.use(new GoogleStrategy({
-        clientID: "----",//CLIENT_ID,
-        clientSecret: "----", //CLIENT_SECRET,
-        //callbackURL: 'https://localhost:3000/auth/google/callback'
-        callbackURL: 'https://cimsmed.dev/auth/google/callback'
-      },
+passport.use(new GoogleStrategy({
+  clientID: "----",//CLIENT_ID,
+  clientSecret: "----", //CLIENT_SECRET,
+  //callbackURL: 'https://localhost:3000/auth/google/callback'
+  callbackURL: 'https://cimsmed.dev/auth/google/callback'
+  passReqToCallback   : true
+},
       
-        
-        function(token, refreshToken, profile, done) {
 
-            // make the code asynchronous
-            // User.findOne won't fire until we have all our data back from Google
-            process.nextTick(function() {
+  function(token, refreshToken, profile, done) {
 
-                // try to find the user based on their google id
-                User.findOne({ 'google.id' : profile.id }, function(err, user) {
-                    if (err)
-                        return done(err);
+    // make the code asynchronous
+    // User.findOne won't fire until we have all our data back from Google
+    process.nextTick(function() {
 
-                    if (user) {
+      // try to find the user based on their google id
+      User.findOne({ 'google.id' : profile.id }, function(err, user) {
+        if (err)
+          return done(err);
 
-                        // if a user is found, log them in
-                        return done(null, user);
-                    } else {
-                        // if the user isnt in our database, create a new user
-                        var newUser          = new User();
+        if (user) {
 
-                        // set all of the relevant information
-                        newUser.google.id    = profile.id;
-                        newUser.google.token = token;
-                        newUser.google.name  = profile.displayName;
-                        newUser.google.email = profile.emails[0].value; // pull the first email
+              // if a user is found, log them in
+          return done(null, user);
+        } else {
+          // if the user isnt in our database, create a new user
+          var newUser          = new User();
 
-                        // save the user
-                        newUser.save(function(err) {
-                            if (err)
-                                throw err;
-                            return done(null, newUser);
-                        });
-                    }
-                });
-            });
+          // set all of the relevant information
+          newUser.google.id    = profile.id;
+          newUser.google.token = token;
+          newUser.google.name  = profile.displayName;
+          newUser.google.email = profile.emails[0].value; // pull the first email
 
-        }));
+          // save the user
+          newUser.save(function(err) {
+            if (err)
+              throw err;
+            return done(null, newUser);
+          });
+        };
+      });
+    });
+
+  }));
 
 };
 
