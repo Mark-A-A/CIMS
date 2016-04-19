4cef634f0cd7ecfdaf3cd036dfb5a42ea24b3102
Servio <servio19@gmail.com>
2016-04-19 17:15:19 -0400

    merged develop

--- controller/authenticate.js
+++ controller/authenticate.js
@@ -2,11 +2,15 @@ var express = require('express');
 var logout = require('express-passport-logout');
 var router = express.Router();
 var mongojs = require('mongojs');
-var eventDb = mongojs("cims-db",["Events"]);
+// var eventDb = mongojs("cims-db",["events"]);
+// var Event = require('../model/events.js');
+var db = require('../config/db.js');
+var mongoose = require('mongoose');
+var Event = mongoose.model('Event');
 
-eventDb.on('error',function(err){
-  console.log('database error'+err);
-});
+// eventDb.on('error',function(err){
+//   console.log('database error'+err);
+// });
 
 module.exports = function (passport) {
 
@@ -50,17 +54,44 @@ module.exports = function (passport) {
 
   router.get('/populateCalendar/:id',function(req,res,next){
     var doctorId = req.params.id;
-
-   eventDb.Events.find({doctorId: doctorId}, function(err, documents){
+    // console.log("doctor id is :"+doctorId);
+    // var newEvent = new Event();
+    Event.find({drIdentifier: doctorId}, function(err, documents){
         if(err){
             console.log(err);
         } else {
-          console.log("Pulled Calendar for the doctor"+doctorId);
+          // console.log("Pulled Calendar for the doctor"+doctorId);
             res.json(documents);
         }
     })
   });
 
+  router.post('/addEvent',function(req,res,next){
+    // console.log("In addEvent function");
+    // console.log(req.body.appointment);
+      var newEvent = new Event();
+
+      newEvent.drIdentifier =  req.body.appointment.drIdentifier,
+      newEvent.name = req.body.appointment.name,
+      newEvent.eventStartsAt = req.body.appointment.eventStartsAt,
+      newEvent.eventEndsAt = req.body.appointment.eventEndsAt,
+      newEvent.aggree = req.body.appointment.aggree,
+      newEvent.agreeSign = req.body.appointment.agreeSign,
+      newEvent.email = req.body.appointment.email,
+      newEvent.gender = req.body.appointment.gender,
+      newEvent.phone = req.body.appointment.phone,
+
+      // save the Event
+      newEvent.save(function(err) {
+        if (err){
+          console.log('Error in Saving the Event: '+err);
+          throw err;
+        }
+        // console.log("Event Successfully saved");
+      });
+
+     res.json({});
+  });
 // GET /auth/google
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  The first step in Google authentication will involve
